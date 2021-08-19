298bc381eb66bd74669fec3c7f92573474d5f790
BrianOrtizSE <77475041+BrianOrtizSE@users.noreply.github.com>
2021-08-19 18:01:55 -0500

    Input Output for C#

---
+++ BOrtizA2_2IO/BOrtizA2_2IO/frmMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace BOrtizA2_2IO
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxList = new System.Windows.Forms.ListBox();
+            this.btnSales = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbxList
+            // 
+            this.lbxList.FormattingEnabled = true;
+            this.lbxList.Location = new System.Drawing.Point(12, 12);
+            this.lbxList.Name = "lbxList";
+            this.lbxList.Size = new System.Drawing.Size(136, 134);
+            this.lbxList.TabIndex = 0;
+            // 
+            // btnSales
+            // 
+            this.btnSales.Location = new System.Drawing.Point(12, 166);
+            this.btnSales.Name = "btnSales";
+            this.btnSales.Size = new System.Drawing.Size(53, 23);
+            this.btnSales.TabIndex = 1;
+            this.btnSales.Text = "&Sales";
+            this.btnSales.UseVisualStyleBackColor = true;
+            this.btnSales.Click += new System.EventHandler(this.btnSales_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(81, 166);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(67, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSales);
+            this.Controls.Add(this.lbxList);
+            this.Name = "frmMain";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mouse_click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxList;
+        private System.Windows.Forms.Button btnSales;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ BOrtizA2_2IO/BOrtizA2_2IO/frmMain.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace BOrtizA2_2IO
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+           
+            InitializeComponent();
+            
+        }
+        StreamReader sr = new StreamReader("sales.txt");
+        StreamWriter sw = new StreamWriter("out.txt");
+        double[] nums = new double[9];
+        
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = double.Parse(sr.ReadLine());
+                lbxList.Items.Add(nums[i].ToString());
+            }
+        }
+
+        private void mouse_click(object sender, MouseEventArgs e)
+        {
+            if(e.X > 250 && e.Y > 250){
+                MessageBox.Show("Clicked In Lower Right Quadrant");
+            }else if(e.X <= 250 && e.Y <= 250)
+            {
+                MessageBox.Show("Clicked In Top Left Quandrant");
+            }
+            else if(e.X > 250 && e.Y <= 250)
+            {
+                MessageBox.Show("Clicked In Top Right Quandrant");
+            }
+            else if (e.X <= 250 && e.Y > 250)
+            {
+                MessageBox.Show("Clicked In Lower Left Quandrant");
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSales_Click(object sender, EventArgs e)
+        {
+            double total = 0.0;
+            double average = 0.0;
+            double big = nums[0];
+            double small = nums[0];
+
+            foreach (double n in nums)
+            {
+                total += n;
+                              
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(small > nums[i])
+                {
+                    small = nums[i];
+                }
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (big < nums[i])
+                {
+                    big = nums[i];
+                }
+            }
+            average = total / nums.Length;
+
+            sw.WriteLine("Total : " + total.ToString());
+            sw.WriteLine("Average : " + average.ToString("n2"));
+            sw.WriteLine("Biggest Number : " + big.ToString());
+            sw.WriteLine("Smallest Number : " + small.ToString());
+            sw.Close();
+        }
+    }
+}
